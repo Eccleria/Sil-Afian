c98f02c7e5c40c19e6dbb9641cf349980cf45276
Eccléria <97692338+Eccleria@users.noreply.github.com>
2025-06-22 17:36:38 +0200

    [DRAFT] octagonalButtonHandler

--- src/commands/utils.js
+++ src/commands/utils.js
@@ -1,6 +1,7 @@
 import { ButtonBuilder } from "discord.js";
 import { interactionReply } from "ewilib";
 
+import { octagonalButtonHandler } from "../admin/octagonal.js";
 import { announceButtonHandler } from "./announce.js";
 
 /**
@@ -25,6 +26,7 @@ export const createButton = (id, label, style, emoji) => {
 export const buttonHandler = (interaction) => {
   const { customId } = interaction;
   if (customId.startsWith("announce")) announceButtonHandler(interaction);
+  else if (customId.startsWith("octagonal")) octagonalButtonHandler(interaction);
   else interactionReply(interaction, "ERROR 404");
 };
 
